2e01480c05b49bfd3a6ff6ac835cb15138dfdf74
TagandurdyB <TBMtkm2003@gmail.com>
2023-02-22 20:02:18 +0300

    0.0.1

---
+++ ViewModel/routes_vm.dart
@@ -0,0 +1,15 @@
+import '/View/Pages/page_home.dart';
+import 'package:flutter/material.dart';
+
+import '../View/Pages/page_logo.dart';
+
+class Rout {
+  // Home never change!!!
+  static const String home = "/HomePage";
+  static const String logo = "/LogoPage";
+
+  static Map<String, Widget Function(dynamic)> pages = {
+    logo: (context) => const LogoPage(),
+    home: (context) => const HomePage(),
+  };
+}
